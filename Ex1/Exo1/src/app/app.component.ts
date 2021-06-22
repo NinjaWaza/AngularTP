80b54ec7ad52cc74417faf9ea328fa9daff5301f
Théodore <theodore.follet@gmail.com>
2021-06-22 14:56:38 +0200

    Added the first component

--- Ex1/Exo1/src/app/app.component.ts
+++ Ex1/Exo1/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <input type="text" id="firstInput" name="firstInput" [(ngModel)]="inputValue">
+    <br /><br />
+    <span style="border: 1px solid black">{{inputValue}}</span>
+    <br /><br />
+    <button (click)="inputValue = ''">Clear</button>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Exo1';
+  inputValue : String;
 }
